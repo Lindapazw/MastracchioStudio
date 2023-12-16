ac26e5b50b6344248eccf385de448a9c1189104b
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-16 14:24:43 -0300

    add Header Scroll

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,48 @@
-import React from 'react'
+"use client"
+
+import {useContext, useEffect, useState} from 'react';
+
+// next image
+import Image from 'next/image';
+
+// react scroll
+import {Link} from 'react-scroll';
+
+// components
+import SearchMobile from './SearchMobile';
+
+// media query hook
+import {useMediaQuery} from 'react-responsive';
 
 const Header = () => {
+    const [header, setHeader] = useState(false);
+    const [nav, setNav] = useState(false);
+
+    const desktopMode = useMediaQuery({
+        query: '(min-width: 1300px)',
+    });
+
+    useEffect(()=>{
+        const handleScroll = () => {
+            // header
+            if(window.scrollY > 40 ) {
+                setHeader(true);
+            } else {
+                setHeader(false);
+            }
+        };
+
+        // add event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // remove event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll )
+        };
+    });
+
     return (
-        <div>Header</div>
+        <header className={`${header} 'bg-white shadow-md py-2' : 'bg-transparent shadow-none py-4' `}> Header </header>
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className='max-w-[1920px] bg-white mx-auto relative overflow-hidden'>
       <Header/>
       <Hero/>
-
+      <div className='h-[4000px]'></div>
     </main>
   )
 }
