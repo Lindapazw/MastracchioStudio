baac5275b29f19a7e9cf0e99573346c54de648b6
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-17 12:26:57 -0300

    add logo in Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -14,6 +14,9 @@ import SearchMobile from './SearchMobile';
 // media query hook
 import {useMediaQuery} from 'react-responsive';
 
+// icons
+import {BiMenuAltRight, BiX} from 'react-icons/bi'
+
 const Header = () => {
     const [header, setHeader] = useState(false);
     const [nav, setNav] = useState(false);
@@ -42,7 +45,24 @@ const Header = () => {
     });
 
     return (
-        <header className={`${header} 'bg-white shadow-md py-2' : 'bg-transparent shadow-none py-4' `}> Header </header>
+        <header className={`${header ? 'bg-white shadow-md py-0' : 'bg-transparent shadow-none py-1'} fixed w-full max-w-[1920] mx-auto z-20 transition-all duration-300`}> 
+            <div className='xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-center xl:justify-between'>
+                <div className='flex justify-between items-center px-4'>
+                    {/* logo */}
+                    <Link to='home' smooth={desktopMode} spy={true} className='cursor-pointer'>
+                        <Image
+                            src="/logo/mastracchio_logo.png"
+                            width={144}
+                            height={34}
+                            alt="logo"
+                            loading="lazy"
+                        />
+                    </Link>
+                    {/* nav open menu */}
+                    <div>Menu</div>
+                </div>
+            </div>
+        </header>
     )
 }
 
