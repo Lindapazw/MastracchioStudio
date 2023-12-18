4230db90ab7553789819cc63087bb4f7fdd836ad
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 16:26:07 -0300

    add image in About section

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,8 +1,28 @@
+"use client"
 
+// image
+import Image from 'next/image';
+
+// framer motion
+import {motion} from 'framer-motion'
+
+// variants
+import { fadeIn } from '/variants';
 
 const About = () => {
     return (
-        <section className="section flex items-center bg-pink-500" id="about">About</section>
+        <section className="section flex items-center" id="about">
+            <div className='container mx-auto'>
+                <div className='flex flex-col xl:flex-row xl:justify-between'>
+                    {/* image */}
+                    <div>
+                        <Image src={'/about/3.png'} width={600} height={448} alt='about image' loading="lazy" quality={80}/>
+                    </div>
+                    {/* text  */}
+                    <div>text </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
