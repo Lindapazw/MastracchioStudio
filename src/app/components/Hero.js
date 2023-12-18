b61d2d029be79fd8ead5afce8a31e5f6b9a1334c
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 14:01:03 -0300

    add video in Hero.js

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,13 +1,20 @@
-"use cliente"
-
-// Components
-
 const Hero = () => {
     return (
-        <section className="h-screen  flex justify-center items-end" id="home">
-            <div className="h-[89vh] bg-slate-500 w-full">hero</div>
+        <section className="h-screen flex justify-center items-end" id="home">
+        <div className="h-[89vh] w-full">
+            <video
+                autoPlay
+                muted
+                loop
+                className="w-full h-full object-cover"
+                loading="lazy"
+            >
+                <source src="/videos/chicaAlta.mp4" type="video/mp4" />
+                Tu navegador no soporta el elemento de video.
+            </video>
+        </div>
         </section>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
