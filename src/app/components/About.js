384c6d370645daacd4582eedb581ed8ded428e83
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 17:03:45 -0300

    add and optimized About.js

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -10,20 +10,37 @@ import {motion} from 'framer-motion'
 import { fadeIn } from '/variants';
 
 const About = () => {
-    return (
-        <section className="section flex items-center" id="about">
-            <div className='container mx-auto'>
-                <div className='flex flex-col xl:flex-row xl:justify-between'>
-                    {/* image */}
-                    <div>
-                        <Image src={'/about/3.png'} width={600} height={448} alt='about image' loading="lazy" quality={80}/>
-                    </div>
-                    {/* text  */}
-                    <div>text </div>
+return (
+    <section className="section flex items-center" id="about">
+        <div className="container mx-auto">
+            <div className="flex flex-col xl:flex-row xl:justify-between">
+            {/* Image */}
+            <div className="p-2 xl:p-0">
+                <Image
+                    src="/about/3.png"
+                    width={500}
+                    height={418}
+                    alt="about image"
+                    loading="lazy"
+                    quality={80}
+                    format="webp" 
+                />
+            </div>
+            {/* Text & Stats */}
+            <div className="flex-1 flex items-center xl:justify-center">
+                <div className="xl:max-w-[517px]">
+                <h2 className="h2 mt-4 xl:mt-0">Un poquito sobre mí</h2>
+                <p>
+                    Mastracchio es el apellido italiano de mi abuela, y esta marca busca capturar todo el arte con el que ella enriqueció mi vida desde mi infancia. <br /> <br />
+                    Mi abuela me legó un patrimonio artístico que dejó una profunda huella en mi creatividad. Esto me llevó a graduarme como arquitecta y a fundar mi propio emprendimiento, ofreciendo una amplia gama de productos, desde objetos de decoración y blanquería hasta indumentaria y accesorios, entre muchas otras cosas. <br /> <br />
+                    Este es mi homenaje a ella, María Teresa Mastracchio. ¡Gracias por tanto!
+                </p>
                 </div>
             </div>
-        </section>
-    )
-}
+            </div>
+        </div>
+    </section>
+    );
+};
 
-export default About
+export default About;
