f018c8829e59773bddf6d8ad2afdfada96c302b0
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 13:00:13 -0300

    add Hero size

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,8 +1,12 @@
-import React from 'react'
+"use cliente"
+
+// Components
 
 const Hero = () => {
     return (
-        <div></div>
+        <section className="h-screen  flex justify-center items-end" id="home">
+            <div className="h-[89vh] bg-slate-500 w-full">hero</div>
+        </section>
     )
 }
 
