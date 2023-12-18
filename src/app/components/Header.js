fba385040eee394ddee55a23388b866864147011
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 19:04:38 -0300

    add footer

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
             <div className='xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-center xl:justify-between'>
                 <div className='flex justify-between items-center px-4'>
                     {/* logo */}
-                    <Link to='home' smooth={desktopMode} spy={true} className='cursor-pointer'>
+                    <Link to='home' smooth={desktopMode} spy={true} className='cursor-pointer border-none'>
                         <Image
                             src="/logo/mastracchio_logo.png"
                             width={144}
@@ -76,11 +76,12 @@ const Header = () => {
                 <nav className={`${nav? 'max-h-max py-8 px-4 xl:py-0 xl:px-0' : 'max-h-0 xl:max-h-max'} flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bold xl:font-bold xl:flex-row xl:w-max xl:gap-x-8 xl:bg-transparent xl:pb-0 transition-all duration-150 text-center xl:text-left uppercase text-sm xl:text-[15px] xl:normal-case`}>
                     <Link className='cursor-pointer' to='home' activeClass='active' smooth={desktopMode} spy={true}>Inicio</Link>
                     <Link className='cursor-pointer' to='about' activeClass='active' smooth={desktopMode} spy={true}>Mastracchio</Link>
-                    <Link className='cursor-pointer' to='testimonials' activeClass='active' smooth={desktopMode} spy={true}>Clientes</Link>
-                    <Link className='cursor-pointer' to='/' activeClass='active' smooth={desktopMode} spy={true}>Redes</Link>
-                    <Link className='cursor-pointer' to='/' activeClass='active' smooth={desktopMode} spy={true}>Link</Link>
-                    <Link className='xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto' to='/' activeClass='active' smooth={desktopMode} spy={true}> Contactanos </Link>
-                    <Link className='cursor-pointer' to='/' activeClass='active' smooth={desktopMode} spy={true}>Link</Link>
+                    <Link className='cursor-pointer' to='services' activeClass='active' smooth={desktopMode} spy={true}>Servicios</Link>
+                    <Link className='cursor-pointer' to='courses' activeClass='active' smooth={desktopMode} spy={true}>Cursos</Link>
+                    <Link className='cursor-pointer' to='testimonial' activeClass='active' smooth={desktopMode} spy={true}>Testimonios</Link>
+                    <Link className='cursor-pointer' to='faqs' activeClass='active' smooth={desktopMode} spy={true}>Preguntas</Link>
+                    <Link className='cursor-pointer' to='contact' activeClass='active' smooth={desktopMode} spy={true}>Contacto</Link>
+                    <Link className='xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto' to='contact' activeClass='active' smooth={desktopMode} spy={true}> Contactanos </Link>
                 </nav>
             </div>
         </header>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
+import Services from './components/Services';
+import Courses from './components/Courses';
+import Testimonial from './components/Testimonial';
+import Faqs from './components/Faqs';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Copyright from './components/Copyright';
 
 
 export default function Home() {
@@ -10,6 +17,13 @@ export default function Home() {
       <Header/>
       <Hero/>
       <About/>
+      <Services/>
+      <Courses/>
+      <Testimonial/>
+      <Faqs/>
+      <Contact/>
+      <Footer/>
+      <Copyright/>
       <div className='h-[4000px]'></div>
     </main>
   )
