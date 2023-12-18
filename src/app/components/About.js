29577d683d3e3d3e2f00e84853d05d26ddd970a4
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 20:13:28 -0300

    add motion in About section

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -20,7 +20,8 @@ return (
             initial='hidden'
             whileInView={'show'}
             viewport={{once: false, amount: 0.6}}
-            className="p-2 xl:p-0">
+            className="p-2 xl:p-0"
+            >
                 <Image
                     src="/about/about.webp"
                     width={500}
@@ -34,12 +35,25 @@ return (
             {/* Text & Stats */}
             <div className="flex-1 flex items-center xl:justify-center">
                 <div className="xl:max-w-[517px]">
-                <h2 className="h2 mt-4 xl:mt-0">Un poquito sobre mí</h2>
-                <p>
+                <motion.h2 
+                variants={fadeIn('down', 0.4)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once: false, amount: 0.4}}
+                className="h2 mt-4 xl:mt-0"
+                >
+                    Un poquito sobre mí
+                </motion.h2>
+                <motion.p
+                variants={fadeIn('down', 0.6)}
+                initial='hidden'
+                whileInView={'show'}
+                viewport={{once: false, amount: 0.4}}
+                >
                     Mastracchio es el apellido italiano de mi abuela, y esta marca busca capturar todo el arte con el que ella enriqueció mi vida desde mi infancia. <br /> <br />
-                    Mi abuela me legó un patrimonio artístico que dejó una profunda huella en mi creatividad. Esto me llevó a graduarme como arquitecta y a fundar mi propio emprendimiento, ofreciendo una amplia gama de productos, desde objetos de decoración y blanquería hasta indumentaria y accesorios, entre muchas otras cosas. <br /> <br />
+                    Mi abuela me legó un patrimonio artístico que dejó una profunda huella en mi creatividad. Esto me llevó a graduarme como arquitecta y a fundar mi propio emprendimiento, ofreciendo una amplia gama de productos, desde objetos de decoración, entre muchas otras cosas. <br /> <br />
                     Este es mi homenaje a ella, María Teresa Mastracchio. ¡Gracias por tanto!
-                </p>
+                </motion.p>
                 </div>
             </div>
             </div>
