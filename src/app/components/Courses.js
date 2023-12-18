949fbaa4e1f66590533237c036f69ad38ddd0a42
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-18 18:57:20 -0300

    add new sections

---
+++ src/app/components/Courses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Courses = () => {
+    return (
+        <div id='couses'>Courses</div>
+    )
+}
+
+export default Courses
---
+++ src/app/components/Faqs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Faqs = () => {
+    return (
+        <div id='faqs'>Faqs</div>
+    )
+}
+
+export default Faqs
