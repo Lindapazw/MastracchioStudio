8084bc8e917f90cb40a048eadc42f3f253db48d4
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-19 10:20:38 -0300

    add testimonialSlider

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -1,7 +1,13 @@
+// components
+import TestimonialSlider from './TestimonialSlider';
 
 const Testimonial = () => {
     return (
-        <section className="section flex items-center bg-red-400" id="testimonial">Testimonial</section>
+        <section className="section flex items-center bg-red-400" id="testimonial">
+            <div className='container mx-auto'>
+                <TestimonialSlider/>
+            </div>
+        </section>
     )
 }
 
