d6bbc24c810af1318444e8dc1f930b8fbcfbda1c
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-20 15:59:58 -0300

    add images

--- src/app/components/Courses.js
+++ src/app/components/Courses.js
@@ -1,36 +1,82 @@
-// next image
+"use client"
+
+// image
 import Image from 'next/image';
+
+// framer motion
+import {motion} from 'framer-motion'
+
+// variants
+import { fadeIn } from '/variants';
 
 const Courses = () => {
     return (
-        <div className='section flex items-center justify-center' id='courses'>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-                <div class="bg-gray-200 p-4 rounded">
-                    <Image 
-                        src={'/about/about.png'} 
-                        width={504} 
-                        height={554} 
-                        alt='avatar de tertimoniales' 
-                        className='mb-4'
+        <section className="section flex items-center" id="courses">
+        <div className='container mx-auto xl:p-20 xl:mt-20'>
+            <div class="grid gap-4 grid-cols-2 xl:grid-cols-4 h-screen">
+                <div class="col-span-2">
+                    <Image
+                        src="/cursos/25.png"
+                        width={500}
+                        height={418}
+                        alt="about image"
                         loading="lazy"
                         quality={80}
-                        format="webp" 
+                        format="webp"
+                        className="object-cover h-full w-full"
                     />
                 </div>
-                <div class="bg-gray-200 p-4 rounded">
-                    <Image 
-                        src={'/about/about.png'} 
-                        width={504} 
-                        height={554} 
-                        alt='avatar de tertimoniales' 
-                        className='mb-4'
+                <div class="xl:row-span-2">
+                    <Image
+                        src="/cursos/15.png"
+                        width={500}
+                        height={418}
+                        alt="about image"
                         loading="lazy"
                         quality={80}
                         format="webp" 
+                        className="object-cover h-full w-full"
+                    />
+                </div>
+                <div class="xl:row-span-2">
+                    <Image
+                        src="/cursos/20.png"
+                        width={500}
+                        height={418}
+                        alt="about image"
+                        loading="lazy"
+                        quality={80}
+                        format="webp" 
+                        className="object-cover h-full w-full"
+                    />
+                </div>
+                <div>
+                    <Image
+                        src="/cursos/17.png"
+                        width={500}
+                        height={418}
+                        alt="about image"
+                        loading="lazy"
+                        quality={80}
+                        format="webp" 
+                        className="object-cover h-full w-full"
+                    />
+                </div>
+                <div>
+                    <Image
+                        src="/cursos/21.png"
+                        width={500}
+                        height={418}
+                        alt="about image"
+                        loading="lazy"
+                        quality={80}
+                        format="webp" 
+                        className="object-cover h-full w-full"
                     />
                 </div>
             </div>
         </div>
+        </section>
     )
 }
 
