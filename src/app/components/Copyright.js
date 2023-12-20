312de63644362fbd8a026bda1582226af9c6bb8b
Linda Paz <lindacsndelapaz@gmail.com>
2023-12-20 16:02:10 -0300

    fix copyright

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -3,7 +3,7 @@
 const Copyright = () => {
     return (
         <div className="text-center py-10 border-t text-sm">
-            Copyright &copy; Lindapazw 2023. Todos los derechjos reservados.
+            Copyright &copy; Lindapazw 2023.
         </div>
     )
 }
